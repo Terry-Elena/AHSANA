47db244325c283b96858da2a347efef4c14c65ef
Terry-Elena <enklesh@gmail.com>
2019-05-31 11:05:14 +0300

    Первая загрузка

---
+++ js/mainb.js
@@ -0,0 +1,36 @@
+  var navMain = document.querySelector('.main-nav');
+  var navToggle = document.querySelector('.main-nav__toggle');
+
+  navMain.classList.remove('main-nav--nojs');
+
+  navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+    } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+    }
+  });
+
+
+$(document).ready(function(){
+    // Плавная прокрутка
+    $('.go_to').click( function(){ // ловим клик по ссылке с классом go_to
+        var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+            $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); // анимируем скроолинг к элементу scroll_el
+        }
+        return false; // выключаем стандартное действие
+    });
+
+    // Модальное окно
+    $('.popup').click(function () {
+        $('#exampleModal').arcticmodal();
+     
+    });
+
+     });
+
+var fullimage = document.querySelector('img.news-image');
+
